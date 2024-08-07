45e0b56bf9143ed59f38bf86065ba8461955d562
4li77l3n00b <crazythor@126.com>
2024-08-08 07:09:49 +0800

    "Aluminium" Quickfix

--- kubejs/startup_scripts/Materials.js
+++ kubejs/startup_scripts/Materials.js
@@ -1,30 +1,30 @@
 //priority: 1
 const $BloomProperty = Java.loadClass("com.gbth.gbthcore.gtceu.material.BloomProperty");
-const PropertyKey = Java.loadClass('com.gregtechceu.gtceu.api.data.chemical.material.properties.PropertyKey')
-const OreProperty = Java.loadClass('com.gregtechceu.gtceu.api.data.chemical.material.properties.OreProperty')
-
 
 GTCEuStartupEvents.registry("gtceu:material", (event) => {
+  const PropertyKey = Java.loadClass('com.gregtechceu.gtceu.api.data.chemical.material.properties.PropertyKey')
+  const OreProperty = Java.loadClass('com.gregtechceu.gtceu.api.data.chemical.material.properties.OreProperty')
   //Add materials
   event.create('alpha_alumina')
     .color(0xFFFFFF)
     .secondaryColor(0xF0F0F0)
     .dust()
     .iconSet(GTMaterialIconSet.DULL)
-    .components('2x aluminum', '3x oxygen')
+    .components('2x aluminium', '3x oxygen')
   
   event.create('gamma_alumina')
     .color(0xFFFFFF)
     .secondaryColor(0xF0F0F0)
     .dust()
     .iconSet(GTMaterialIconSet.DULL)
-    .components('2x aluminum', '3x oxygen')
+    .components('2x aluminium', '3x oxygen')
 
   event.create('cryolite')
     .color(0xfffae6)
     .secondaryColor(0xfff2e6)
     .gem()
-    .components('1x sodium', '3x aluminum', '6x fluorine')
+    .components('1x sodium', '3x aluminium', '6x fluorine')
+    .iconSet(GTMaterialIconSet.DULL)
     ['fluid(com.gregtechceu.gtceu.api.fluids.store.FluidStorageKey,com.gregtechceu.gtceu.api.fluids.FluidBuilder)'](GTFluidStorageKeys.LIQUID, new GTFluidBuilder()
       .temperature(1012+273)
       .density(3.032)
