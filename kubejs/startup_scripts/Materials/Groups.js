c7f36d68db7d1c52dd8736b92a72dad07785f20b
4li77l3n00b <crazythor@126.com>
2024-08-10 01:47:40 +0800

    quickfix

--- kubejs/startup_scripts/Materials/Groups.js
+++ kubejs/startup_scripts/Materials/Groups.js
@@ -6,29 +6,39 @@ GTCEuStartupEvents.registry("gtceu:material", (event) => {
     // Groups
     event.create('hydroxide')
         .components('1x hydrogen', '1x oxygen')
+        .plasma()
 
     event.create('aluminate')
         .components('1x aluminium', '2x oxygen')
+        .plasma()
     
     event.create('metasilicate')
         .components('1x silicon', '3x oxygen')
+        .plasma()
     event.create('orthosilicate')
         .components('1x silicon', '4x oxygen')
+        .plasma()
     
     event.create('hexafluoroaluminate')
         .components('1x aluminium', '6x fluorine')
+        .plasma()
 
     event.create('sulfate')
         .components('1x sulfur', '4x oxygen')
+        .plasma()
     event.create('sulfite')
         .components('1x sulfur', '3x oxygen')
+        .plasma()
     
     event.create('carbonate')
         .components('1x carbon', '3x oxygen')
+        .plasma()
     
     event.create('phosphate')
         .components('1x phosphorus', '4x oxygen')
+        .plasma()
 
     event.create('aluminosilicate_cage')
         .components('6x aluminium', '6x silicon', '24x oxygen')
+        .plasma()
 })
