04298be5eb637776644c9fd979b52edd9eddb7fb
Goldgom <qym2005723@hotmail.com>
2024-08-09 22:00:09 +0800

    feat:gun bullet recipes

---
+++ kubejs/server_scripts/Mods2/createbigcannons.js
@@ -0,0 +1,9 @@
+ServerEvents.recipes(event => {
+    event.shaped('createbigcannons:packed_gunpowder', [
+        ' P ',
+        'PPP',
+        ' P '
+    ], {
+        P: '#forge:gunpowder'
+    })
+})
